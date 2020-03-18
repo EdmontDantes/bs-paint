9d0a00f7ab8e487a21ee9636c2b81ef8e591e9cf
Bogdan <bogdan@En760.kowaltchook.com>
2020-03-18 15:26:01 -0400

    added five more extra colors from stretch goal

--- main.js
+++ main.js
@@ -64,6 +64,77 @@ const changeBrushWhitePlusCanvas = function() {
 //EventListener for current Brush change
 whiteColor.addEventListener('click', changeBrushWhitePlusCanvas);
 
+
+//Pick mediumSlateBLue Color
+const mediumSlateBlueColor = document.querySelector(".palette-color.color-6");
+
+//Handler Function to change color of Current Brush
+const changeBrushMediumSlateBluePlusCanvas = function() {
+    const brushChange = document.querySelector(".current-brush");
+    brushChange.classList.replace(brushChange.classList[1], 'color-6');
+
+}
+
+//EventListener for current Brush change
+mediumSlateBlueColor.addEventListener('click', changeBrushMediumSlateBluePlusCanvas);
+
+
+//Pick AquaMarine Color
+const aquaMarineColor = document.querySelector(".palette-color.color-7");
+
+//Handler Function to change color of Current Brush
+const changeBrushAquaMarinePlusCanvas = function() {
+    const brushChange = document.querySelector(".current-brush");
+    brushChange.classList.replace(brushChange.classList[1], 'color-7');
+
+}
+
+//EventListener for current Brush change
+aquaMarineColor.addEventListener('click', changeBrushAquaMarinePlusCanvas);
+
+
+//Pick brown Color
+const brownColor = document.querySelector(".palette-color.color-8");
+
+//Handler Function to change color of Current Brush
+const changeBrushBrownPlusCanvas = function() {
+    const brushChange = document.querySelector(".current-brush");
+    brushChange.classList.replace(brushChange.classList[1], 'color-8');
+
+}
+
+//EventListener for current Brush change
+brownColor.addEventListener('click', changeBrushBrownPlusCanvas);
+
+//Pick seaGreen Color
+const seaGreenColor = document.querySelector(".palette-color.color-9");
+
+//Handler Function to change color of Current Brush
+const changeBrushseaGreenPlusCanvas = function() {
+    const brushChange = document.querySelector(".current-brush");
+    brushChange.classList.replace(brushChange.classList[1], 'color-9');
+
+}
+
+//EventListener for current Brush change
+seaGreenColor.addEventListener('click', changeBrushseaGreenPlusCanvas);
+
+//Pick LimeGreen Color
+const limeGreenColor = document.querySelector(".palette-color.color-10");
+
+//Handler Function to change color of Current Brush
+const changeBrushLimeGreenPlusCanvas = function() {
+    const brushChange = document.querySelector(".current-brush");
+    brushChange.classList.replace(brushChange.classList[1], 'color-10');
+
+}
+
+//EventListener for current Brush change
+limeGreenColor.addEventListener('click', changeBrushLimeGreenPlusCanvas);
+
+
+
+
 //Pick changed Current Brush
 const currentBrushColor = document.querySelector(".current-brush");
 
@@ -71,6 +142,8 @@ const currentBrushColor = document.querySelector(".current-brush");
 const squaresChangeColor = function (event) {
     event.target.classList.replace(event.target.classList[1], currentBrushColor.classList[1]);
 }
+
+
 
 //Pick square query
 const canvasSquares = document.querySelectorAll(".square");
